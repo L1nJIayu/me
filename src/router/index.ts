cb75722455f2900edd109b083d7a5c6a94647e99
L1nJIayu <562316920@qq.com>
2024-01-10 22:14:06 +0800

    :-D

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import ListOf100Days from '@/views/100days/ListOf100Days.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '100days'
+    },
+    {
+      path: '/100days',
+      name: '100days',
+      component: () => import('@/views/100days/ListOf100Days.vue')
+    },
+    {
+      path: '/itemOf100days',
+      name: 'itemOf100days',
+      component: () => import('@/views/100days/ItemOf100Days.vue'),
+      children: [
+        {
+          path: ':001',
+          name: '001',
+          component: () => import('@/views/100days/Mass.vue')
+        },
+      ]
+    }
+  ]
+})
+
+export default router
---
+++ src/views/100days/ItemOf100Days.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <div @click="back">&lt;</div>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView, useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const back = () => router.go(-1)
+</script>
+
+<style scoped>
+</style>
